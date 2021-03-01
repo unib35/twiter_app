1d9d72c0f4e66d557aeac8c30a389695961f9c51
lee <unib335@naver.com>
2021-03-01 01:44:16 +0000

    Log out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
+import Profile from '../routes/Profile';
+import Navigation from 'components/Navigation'
 
 const AppRouter = (props) => {	
 	
 	return (
 		<Router>
+			{/*Navigation이 실행되려면 props.Logged가 true 여야 된다.*/}
+			{props.Logged && <Navigation />}
 			<Switch>
 				{props.Logged ? (
 					<>
 						<Route exact path="/">
 							<Home />
 						</Route>
+						<Route exact path="/profile">
+						<Profile />
+						</Route>
+						<Redirect from='*' to ='/' />
 					</>
 				) : ( 
+					<>
 					<Route exact path="/">
 						<Auth />
-					</Route> 
+					</Route>
+					<Redirect from='*' to ='/' />
+					</>
 				)}
 			</Switch>
 		</Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { authService } from '../fbase'
+import { useHistory } from 'react-router-dom'
 
 const Profile = () => {
-	<span>Profile</span>
+	
+	const history = useHistory();
+	const LogoutHandler = () => {
+		authService.signOut()
+		history.push('/')
+		alert('Success To Logout')
+	}
+	
+	return (
+		<>
+			<button onClick={LogoutHandler}>Log out</button>
+		</>
+	)
 }
 
 
